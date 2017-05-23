b31deb35896741fb11ee6c297eadaeae31af5914
dilshani <dilshani@wso2.com>
2017-05-23 17:15:20 +0530

    Updated esb-perf-execution.sh with hostnames

--- Instances/JmeterInstance/esb-perf-execution.sh
+++ Instances/JmeterInstance/esb-perf-execution.sh
@@ -25,8 +25,8 @@ JVM_ARGS="-Xms2g -Xmx2g -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps 
 echo "# Running JMeter. Concurrent Users: $2 Service: $4. Duration: $3 JVM Args: $JVM_ARGS"
 ~/apache-jmeter-3.2/bin/jmeter.sh -JConcurrency=$2 -JDuration=$3 -JHost=$1 -JService=$4 -JPayload=$5 -n -t jmeter/ESB_Perf.jmx -l jtl_results/$4_C_$2_T_$3_P_$5.jtl
 ss -s > results/$2/jmeter_ss_$4_C_$2_T_$3_P_$5.txt
-ssh $esbperfesb "ss -s" > results/$2/ei_ss_$2.txt
-ssh $esbperfbackend "ss -s" > results/$2/netty_ss_$2.txt
+ssh esbperfesb "ss -s" > results/$2/ei_ss_$2.txt
+ssh esbperfbackend "ss -s" > results/$2/netty_ss_$2.txt
 
 echo "Warm up period removing..."
 java -jar jtlprocessor/TrimJTL.jar jtl_results/$4_C_$2_T_$3_P_$5.jtl processed_results/$4_C_$2_T_$3_P_$5.jtl 10
